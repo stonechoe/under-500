63c0f0d53b3af7b0e971c85e6dcc989a6bdf906c
Minseok Choe <choems@outlook.com>
2024-11-19 20:48:52 +0900

    chore: add packages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.worker\.(js|ts)$/,
+      use: { loader: 'worker-loader' },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
