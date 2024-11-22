0be58ba5b59963c128dbd2b1366e93b2a68d491e
Minseok Choe <choems@outlook.com>
2024-11-23 01:48:01 +0900

    chore: build script

---
+++ scripts/copy-public.js
@@ -0,0 +1,11 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const src = path.join(__dirname, '..', 'node_modules', 'mediainfo.js', 'dist', 'MediaInfoModule.wasm');
+const dest = path.join(__dirname, '..', 'public', 'MediaInfoModule.wasm.binary');
+
+// Ensure the `public` directory exists
+fs.ensureDir(path.join(__dirname, '..', 'public'))
+  .then(() => fs.copy(src, dest))
+  .then(() => console.log('MediaInfoModule.wasm copied to public directory.'))
+  .catch((err) => console.error('Error copying MediaInfoModule.wasm:', err));
